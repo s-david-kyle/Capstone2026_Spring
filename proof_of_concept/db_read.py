14df200b55ad381f031cd39364e2242ac64d9aaa
Brandon Knox <bknox.dev@gmail.com>
2026-03-23 12:46:07 -0600

    Created conversation and summary retreivals for interface. Summary can be edited, database will update when done so.

--- proof_of_concept/db_read.py
+++ proof_of_concept/db_read.py
@@ -1,8 +1,14 @@
 import sqlite3
 import re
 from config import MODEL
+import pandas as pd
 
-# TODO: remove these functions once you've built new ones
+
+def get_connection():
+    conn = sqlite3.connect('clerkship_dialogue.db')
+    cursor = conn.cursor()
+    return conn, cursor
+
 def get_session_ids():
     """
     Retreives new session IDs for doctor-guided filtering
@@ -13,13 +19,45 @@ def get_session_ids():
     Returns:
         session_ids (list)
     """
-    conn = sqlite3.connect('clerkship_dialogue.db')
-    cursor = conn.cursor()
+    conn, cursor = get_connection()
     # Fetch all values
     cursor.execute("SELECT SessionId FROM Session")
     session_ids = [row[0] for row in cursor.fetchall()]
 
     return session_ids
+
+def get_conversations(session_id):
+    conn, cursor = get_connection()
+    sql = f'''
+        SELECT Message, Speaker
+        FROM Turn 
+        WHERE SessionId = {session_id}
+        '''
+    df = pd.read_sql(sql, conn)
+    return df
+
+def get_summary(session_id):
+    conn, cursor = get_connection()
+    sql = f'''
+        SELECT PreSummary, PostSummary
+        FROM Summary 
+        WHERE SessionId = {session_id}
+        '''
+    df = pd.read_sql(sql, conn)
+    return df
+
+def update_summary(session_id, df):
+    conn, cursor = get_connection()
+    post_summary = df['PostSummary'].values[0]
+    sql = f'''
+        UPDATE Summary
+        SET PostSummary = '{post_summary}'
+        WHERE SessionId = {session_id};
+        '''
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+    
 
 if __name__ == '__main__':
     pass
--- proof_of_concept/doctor.py
+++ proof_of_concept/doctor.py
@@ -6,7 +6,8 @@ from datetime import datetime
 
 # team modules
 from external_data_pull import umls_retrieval
-from db_read import (get_session_ids)
+from db_read import (get_session_ids, get_conversations,
+                     get_summary, update_summary)
 from knowledge_graph import (create_demo_graph)
 
 # ==========================================
@@ -26,10 +27,6 @@ if not os.path.exists(SCRIPT_DIR):
 st.set_page_config(page_title="Doctor Summary", page_icon="🩺")
 st.title("🩺 Doctor Patient Diagnosis")
 
-# knowledge graph
-graph = StreamlitGraphWidget.from_graph(create_demo_graph())
-graph.show()
-
 
 # Sidebar for actions
 st.sidebar.header("Controls")
@@ -39,5 +36,35 @@ option1 = st.sidebar.selectbox(
     'Choose session',
     session_ids
 )
+
+# # create side-by-side views
+# col1, col2 = st.columns(2)
+# with col1:
+#     # conversations
+#     conversations = get_conversations(option1)
+#     st.dataframe(conversations)
+
+# with col2:
+#     # knowledge graph
+#     graph = StreamlitGraphWidget.from_graph(create_demo_graph())
+#     graph.show()
+
+# TODO: stack interface elements for now, until you refine use
+# summary
+summary = get_summary(option1)
+edited_summary = st.data_editor(summary)
+# TODO: write edited_summary to database
+update_summary(option1, edited_summary)
+
+# favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+# st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+# conversations
+conversations = get_conversations(option1)
+st.dataframe(conversations)
+
+# knowledge graph
+graph = StreamlitGraphWidget.from_graph(create_demo_graph())
+graph.show()
 
 # st.sidebar.write('Selected:', option1)
